38ac0ffb9e0a2264a4498cebc5e60b11abde4e85
sasukeamjed <sasukeamjed@gmail.com>
2020-07-03 19:22:31 +0400

    farmers market

---
+++ lib/src/widgets/navbar.dart
@@ -0,0 +1,25 @@
+import 'package:farmersmarket/src/styles/colors.dart';
+import 'package:farmersmarket/src/styles/text.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+abstract class AppNavBar{
+  static CupertinoSliverNavigationBar cupertinoSliverNavBar ({String title, BuildContext context}){
+    return CupertinoSliverNavigationBar(
+      largeTitle: Text(title, style: TextStyles.navTitle,),
+      backgroundColor: Colors.transparent,
+      border: null,
+    );
+  }
+
+  static SliverAppBar materialNavBar ({@required String title, @required TabBar tabBar}){
+    return SliverAppBar(
+      title: Text(title, style: TextStyles.navTitleMaterial,),
+      backgroundColor: AppColors.darkBlue,
+      bottom: tabBar,
+      floating: true,
+      pinned: true,
+      snap: true,
+    );
+  }
+}
